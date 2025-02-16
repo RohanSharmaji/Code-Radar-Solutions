5f2623f40d4e233a92d855248c89369cf6dae0d2
RohanSharmaji <rohan0621.becse24@chitkara.edu.in>
2025-02-16 10:36:27 +0530

    Solution to Logical OR Operation

---
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+ int first;
+ int second;
+ scanf("%d %d",&first,&second);
+ if ((first>0)||(second>0))
+ {
+    printf("True");
+ }
+ else
+ {
+    printf("False");
+ }
+
+}
