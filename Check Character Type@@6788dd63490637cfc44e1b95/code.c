aad8c390249b34f863ada16053d2d4891d30d638
RohanSharmaji <rohan0621.becse24@chitkara.edu.in>
2025-02-16 19:20:49 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  char topa;
+  scanf("%c",&topa);
+  if (topa=='a','e','i','o','u')
+  {
+    printf("Vowel");
+  }
+  else if ((topa>='a')&&(topa<='z'))
+  {
+    printf("Consonant");
+  }
+   
+
+}
